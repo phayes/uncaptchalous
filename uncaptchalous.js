17bc476b8adc0e971dba3887e756d19ff8eae16e
Dustin Currie <dustin_currie@343251.no-reply.drupal.org>
2010-12-05 21:21:32 +0000

    bug report #946806 by xmacinfo: Changed javascript to use Drupal.behaviors instead of .ready() to work with dynamically generated forms.

--- uncaptchalous.js
+++ uncaptchalous.js
@@ -2,6 +2,6 @@
  * @author Dustin Currie
  */
 
-$(document).ready(function () {
+Drupal.behaviors.uncaptchalous = function () {
   $('#edit-uncaptchalous-js-val').attr('value', parseInt($('#edit-uncaptchalous-js-param1').val()) + parseInt($('#edit-uncaptchalous-js-param2').val()));
-});
+};
